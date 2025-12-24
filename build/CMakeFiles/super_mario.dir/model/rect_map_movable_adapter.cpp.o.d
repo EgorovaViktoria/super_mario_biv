00949363ffa9082a77e17643e0e31b4327561f78
EgorovaViktoria <st129122@student.spbu.ru>
2025-12-24 04:44:39 +0300

    1/3 задания точно сделано

--- build/CMakeFiles/super_mario.dir/model/rect_map_movable_adapter.cpp.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/super_mario.dir/model/rect_map_movable_adapter.cpp.o: \
- /home/viktoria/Documents/super_mario/src/model/rect_map_movable_adapter.cpp \
- /usr/include/stdc-predef.h \
- /home/viktoria/Documents/super_mario/src/model/rect_map_movable_adapter.hpp \
- /home/viktoria/Documents/super_mario/src/model/map_movable.hpp \
- /home/viktoria/Documents/super_mario/src/model/rect.hpp \
- /home/viktoria/Documents/super_mario/src/model/coord.hpp
--- build/CMakeFiles/super_mario.dir/ui/console/console_ui_obj_rect_adapter.cpp.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/super_mario.dir/ui/console/console_ui_obj_rect_adapter.cpp.o: \
- /home/viktoria/Documents/super_mario/src/ui/console/console_ui_obj_rect_adapter.cpp \
- /usr/include/stdc-predef.h \
- /home/viktoria/Documents/super_mario/src/ui/console/console_ui_obj_rect_adapter.hpp \
- /home/viktoria/Documents/super_mario/src/ui/console/console_ui_obj.hpp \
- /home/viktoria/Documents/super_mario/src/model/rect.hpp \
- /home/viktoria/Documents/super_mario/src/model/coord.hpp
